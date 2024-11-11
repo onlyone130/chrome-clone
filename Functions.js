7a0166260dd4c2819dd854e5551035e319b84019
tama <rufina130@naver.com>
2024-11-11 14:14:59 +0900

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,17 @@
+//이름 출력
+function sayHello(nameOfPerson){
+    console.log(nameOfPerson);
+}
+
+sayHello("tama");
+sayHello("dal");
+sayHello("lynn");
+
+//이름, 나이 함께 출력
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+
+sayHello("tama", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);

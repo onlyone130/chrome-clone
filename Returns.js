e9bba128edba0ab02139f8d4d09d99ae9cc7e470
tama <rufina130@naver.com>
2024-11-14 10:31:17 +0900

    Update Returns.js

--- Returns.js
+++ Returns.js
@@ -8,3 +8,30 @@ function calculateKrAge(ageOfForeigner){
 const KrAge = calculateKrAge(age);
 
 console.log(KrAge);
+
+
+//calculator with return
+const calculator = {
+    plus: function(a,b){
+        return a+b;
+    },
+    minus: function(a,b){
+        return a-b;
+    },
+    times: function(a,b){
+        return a*b;
+    },
+    divide: function(a,b){
+        return a/b;
+    },
+    power: function(a,b){
+        return a**b;
+    }
+};
+
+//depending codes
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);

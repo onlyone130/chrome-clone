d8215de4514f3b0f2fedce85ae4dab980bb5dae0
tama <rufina130@naver.com>
2024-11-25 13:07:15 +0900

    Update Login_Events.js

--- Login_Events.js
+++ Login_Events.js
@@ -8,3 +8,47 @@ function onLoginSubmit(event){
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
+
+
+//--------------------------------------------------------------------
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    //const username = loginInput.value;
+    console.log(loginInput.value);
+}
+
+function handleLinkClick(){
+    alert("clicked!!!");
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
+
+//------------------------------------------------------------------------
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    //const username = loginInput.value;
+    console.log(loginInput.value);
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+}
+
+//eventListener 안에 있는 함수는 직접 실행하지 않음.
+loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click", handleLinkClick);

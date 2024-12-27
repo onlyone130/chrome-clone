b3233ffb7d594048e24a6743cfb31769dbc5e7eb
tama <rufina130@naver.com>
2024-12-27 14:06:02 +0900

    Update LoadingUsername.js

--- LoadingUsername.js
+++ LoadingUsername.js
@@ -8,20 +8,25 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     //greeting.innerText = "Hello "+ username;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+   paintGreetings(username);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
+function paintGreetings(username){
+    reeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
     //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     //show greetings
+    paintGreetings(savedUsername);
 }

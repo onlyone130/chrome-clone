3d77ec3784d4f9cc3fe338a5e34c4daf274409b6
tama <rufina130@naver.com>
2025-01-11 13:08:26 +0900

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -3,14 +3,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    //console.log("i will paint", newTodo);
+    //html에 들어갈 요소, 태그 생성. 변수명은 li혹은 span 일 필요는 없음
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    //li는 span이라는 child를 가지게됨
+    li.appendChild(span);
+    span.innerText = newTodo;
+    //console.log(li);
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     //입력 후 enter를 입력해도 칸이 초기화되지 않고, 페이지가 새로고침 되지도 않음
     event.preventDefault();
     //step1. input의 현재 value를 새로운 변수에 복사
     const newTodo = toDoInput.value;
-    console.log(toDoInput.vlaue);
+    //console.log(toDoInput.vlaue);
     toDoInput.value = "";
-    console.log(newTodo, toDoInput.value);
+    //console.log(newTodo, toDoInput.value);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
